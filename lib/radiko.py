6972ae4e71a3bb165fcc5f54e12adb0e46cf3b26
Akira Hayakawa <ruby.wktk@gmail.com>
2013-05-03 20:26:13 +0900

    downloading from radiko OK

--- lib/radiko.py
+++ lib/radiko.py
@@ -1,6 +1,9 @@
 import os
 import os.path
 import re
+import commands
+
+import xml.etree.ElementTree as ET
 
 def getCommand1(config):
 	playerurl="http://radiko.jp/player/swf/player_3.0.0.01.swf"
@@ -24,24 +27,24 @@ def getCommand1(config):
 			exit(1)
 			
 	if os.path.exists("auth1_fms"):			
-		os.system("rm auth1_fms")
+		config.R("rm auth1_fms")
 	
 	command = """\
-			wget -q \
-			--header="pragma: no-cache" \
-			--header="X-Radiko-App: pc_1" \
-			--header="X-Radiko-App-Version: 2.0.1" \
-			--header="X-Radiko-User: test-stream" \
-			--header="X-Radiko-Device: pc" \
-			--post-data='\r\n' \
-			--no-check-certificate \
-			--save-headers \
-			https://radiko.jp/v2/api/auth1_fms
-			"""
+wget -q \
+--header="pragma: no-cache" \
+--header="X-Radiko-App: pc_1" \
+--header="X-Radiko-App-Version: 2.0.1" \
+--header="X-Radiko-User: test-stream" \
+--header="X-Radiko-Device: pc" \
+--post-data='\r\n' \
+--no-check-certificate \
+--save-headers \
+https://radiko.jp/v2/api/auth1_fms
+"""
 	r = config.R(command)			
 
 	if (r is not 0) or not os.path.exists("auth1_fms"):
-		config.P("ERROR auth2 failure")
+		config.P("ERROR auth1 failed")
 		exit(1)
 		
 	config.P("auth1 OK")			
@@ -56,7 +59,7 @@ def getCommand1(config):
 	if not m1:
 		config.P("ERROR authtoken not parsed")
 		exit(1)
-	authtoken = m1.group(1)
+	authtoken = m1.group(1).strip()
 	config.P("authtoken: %s" % authtoken)
 
 	p2 = re.compile(r"x-radiko-keyoffset: (.*)", re.I)
@@ -74,5 +77,113 @@ def getCommand1(config):
 		exit(1)
 	keylength = int(m3.group(1))		
 	config.P("keylength: %d" % keylength)
+	
+	command = """ \
+dd \
+if=%s \
+bs=1 \
+skip=%d \
+count=%d \
+2> /dev/null | base64
+""".strip() % (keyfile, keyoffset, keylength)
+	config.P(command)		
+	partialkey = commands.getoutput(command)
+	config.P("partialkey: %s" % (partialkey))
+
+	config.R("rm auth1_fms")
+
+	if os.path.exists("auth2_fms"):
+		config.R("rm auth2_fms")
 		
-	return None
+	command = """ \
+wget -q \
+--header="pragma: no-cache" \
+--header="X-Radiko-App: pc_1" \
+--header="X-Radiko-App-Version: 2.0.1" \
+--header="X-Radiko-User: test-stream" \
+--header="X-Radiko-Device: pc" \
+--header="X-Radiko-Authtoken: %s" \
+--header="X-Radiko-Partialkey: %s" \
+--post-data='\r\n' \
+--no-check-certificate \
+https://radiko.jp/v2/api/auth2_fms
+""".strip() % (authtoken, partialkey)
+	r = config.R(command)
+
+	if (r is not 0) or not os.path.exists("auth2_fms"):
+		config.P("ERROR auth2 failed")
+		exit(1)
+			
+	config.P("auth2 OK")		
+
+	f = open("auth2_fms")
+	text = f.read()
+	config.P(text)
+	f.close()
+
+	areaid = text.split(",")[0].strip()
+	config.P("areaid: %s" % areaid)
+
+	config.R("rm auth2_fms")
+
+	channel = config.args.channel
+	xmlFile = "%s.xml" % channel
+	if os.path.exists(xmlFile):
+		config.R("rm %s" % xmlFile)
+		
+	command = """ \
+wget \
+-q "http://radiko.jp/v2/station/stream/%s"
+""".strip() % xmlFile
+	config.R(command)	
+
+	f = open(xmlFile)		
+	text = f.read()
+	config.P(text)
+	f.close()
+
+	root = ET.fromstring(text)
+	stream_url = root[0].text
+	config.P("stream_url: %s" % stream_url)
+
+	pat = re.compile(r"^(.+)://(.+?)/(.*)/(.*?)$")
+	mat = pat.search(stream_url)
+	A = mat.group(1)
+	B = mat.group(2)
+	C = mat.group(3)
+	D = mat.group(4)	
+	config.P("A: %s, B:%s, C:%s, D:%s" % (A, B, C, D))
+	
+	config.R("rm %s" % xmlFile)
+
+#	command = """ \
+#%s \
+#--rtmp %s \
+#--app %s \
+#--playpath %s \
+#-W %s \
+#-C S:"" -C S:"" -C S:"" -C S:%s \
+#--live \
+#-o %s \
+#--stop %d \
+#""".strip() % (
+#		config.args.rtmpbin,
+#		"%s://%s" % (A, B), C, D,
+#		playerurl, authtoken, config.filename, config.duration_sec)
+
+	command = """ \
+%s \
+--rtmp %s \
+--app %s \
+--playpath %s \
+-W %s \
+-C S:"" -C S:"" -C S:"" -C S:%s \
+--live \
+-o %s \
+--stop %d \
+""".strip() % (
+		config.args.rtmpbin,
+		"%s://%s" % (A, B), C, D,
+		playerurl, authtoken, config.filename, config.duration_sec)
+
+	return command
--- lib/radiru.py
+++ lib/radiru.py
@@ -10,15 +10,15 @@ def getCommand1(config):
 	
 	# make m4a file
 	command1 = """
-	%s \
-	--rtmp "rtmpe://netradio-%s-flash.nhk.jp" \
-	--playpath 'NetRadio_%s_flash@%d' \
-	--app "live" \
-	-W http://www3.nhk.or.jp/netradio/files/swf/rtmpe.swf \
-	--live \
-	-o %s \
-	--stop %d
-	""".strip() % (config.args.rtmpbin, chan, chan.upper(), dial, 
+%s \
+--rtmp "rtmpe://netradio-%s-flash.nhk.jp" \
+--playpath 'NetRadio_%s_flash@%d' \
+--app "live" \
+-W http://www3.nhk.or.jp/netradio/files/swf/rtmpe.swf \
+--live \
+-o %s \
+--stop %d
+""".strip() % (config.args.rtmpbin, chan, chan.upper(), dial, 
 			config.filename, config.duration_sec)
 
 	return command1
