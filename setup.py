8c6bcd7e0095649e10298f567f67b0f8c3144a8c
Akira Hayakawa <ruby.wktk@gmail.com>
2013-05-04 11:06:48 +0900

    modify setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,25 @@ from setuptools import setup
 setup(
 	name = 'radikorec',
 	version = '1.0',
-	description = 'A Simple Radiko Recorder',
+	description = 'A Simple Radiko/Radiru Recorder',
 	author = 'Akira Hayakawa',
 	author_email = 'ruby.wktk@gmail.com',
+	url = 'https://github.com/akiradeveloper/radikorec',
+	keywords = ['radiko', 'radiru', 'radio', 'recorder', 'NHK'],
 	scripts = [
 		'bin/radikorec',
 	],
+	platforms = ['POSIX'],
 	package_dir = {'':'lib'},
 	packages = [''],
 	install_requires = [
 		'argparse >= 1.2',
+	],
+	classifiers = [
+		"Operating System :: POSIX :: Linux",	
+		"Programming Language :: Python",
+		"Development Status :: 4 - Beta",
+		"Environment :: Console",
+		"Topic :: Multimedia",
 	],
 )
